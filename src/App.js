3bedcbcb874e7bf3f32ae93f39d62db30769e231
Mehraj Hasan <mehrajhasancs@gmail.com>
2025-01-03 16:35:43 -0500

    main file adds

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import SearchResults from './pages/SearchResults';
+import Details from './pages/Details';
+
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/search/:query" element={<SearchResults/>}/>
+        <Route path="/details/:id" element={<Details/>}/>
+      </Routes>
+    </Router>
   );
 }
 
